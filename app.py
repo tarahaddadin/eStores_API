ba533973ec452870a7676a4a483ebe6dd626e049
Tara Haddadin <tara_haddadin@yahoo.com>
2020-10-19 10:21:27 +0300

    Fixing a bug related to the db.py file

--- app.py
+++ app.py
@@ -13,10 +13,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'tara'
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 jwt = JWT(app, authenticate, identity)
               
 api.add_resource(Store, '/store/<string:name>')              
@@ -28,5 +24,10 @@ api.add_resource(UserRegister, '/register')
 if __name__ == '__main__':
     from db import db
     db.init_app(app)
-    app.run(port=5000, debug=True)
+        
+    if app.config['DEBUG']:
+        @app.before_first_request
+        def create_tables():
+            db.create_all()
     
+    app.run(port=5000, debug=True)
